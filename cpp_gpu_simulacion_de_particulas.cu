65cfde1700493b79fbbd8462dc11147e54be3411
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2023-06-02 10:07:43 -0300

    Agrego librerías, ctes y la cte alpha en f

--- cpp_gpu_simulacion_de_particulas.cu
+++ cpp_gpu_simulacion_de_particulas.cu
@@ -1,12 +1,31 @@
+#include <iostream>
+#include <fstream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "timer.h"
 
+using namespace std;
+
 #define BLOCK_SIZE 256
 #define SOFTENING 1e-9f
 
 typedef struct { float x, y, vx, vy; } Particula;
+
+//Constantes matemáticas
+const float pi = acos(-1.0);
+
+// Constantes físicas
+const float m = 9.11e-31 * 1e3; // [g]
+const float e = 1.602e-19 * (1 / 3.336641e-10); // [Fr]
+// const float c = 299792458 * 1e2; // [cm/s]
+const float K = 1.380649e-23 * (1 / 1e-7); // constante de Boltzmann [ergio/K], obtenida de NIST
+
+// Radio del círculo y velocidad inicial de la partícula
+const float R0_dim = 1e-6; // [cm]
+const float T0_dim = 300; // [K]
+
+
 
 void randomizeBodies(float *data, int n) {
   for (int i = 0; i < n; i++) {
@@ -15,19 +34,19 @@ void randomizeBodies(float *data, int n) {
 }
 
 __global__
-void bodyForce(Particula *p, Particula *dpdt, float dt, int n) {
+void bodyForce(Particula *p, Particula *dpdt, float dt, int n, float alpha) {
   int i = blockDim.x * blockIdx.x + threadIdx.x;
   if (i < n) {
     float Fx = 0.0f; float Fy = 0.0f;
 
     for (int j = 0; j < n; j++) {
-      float dx = p[j].x - p[i].x;
+      float dx = p[j].x - p[i].x; //o al revés? TO-DO
       float dy = p[j].y - p[i].y;
       float r2 = dx*dx + dy*dy + SOFTENING;
       float inv_r = rsqrtf(r2);
       float inv_r3 = inv_r * inv_r * inv_r;
 
-      Fx += dx * inv_r3; Fy += dy * inv_r3;
+      Fx += alpha * dx * inv_r3; Fy += alpha * dy * inv_r3;
     }
     //Asigno las derivadas
     dpdt[i].x = p[i].vx; dpdt[i].y = p[i].vy;
@@ -37,16 +56,27 @@ void bodyForce(Particula *p, Particula *dpdt, float dt, int n) {
 }
 
 int main(const int argc, const char** argv) {
+  int N = 30000; // Nro de partículas
+  if (argc > 1) N = atoi(argv[1]);
+
+  // Cálculo de las constantes adimensionales
+  float v0_dim = sqrt(3 * K * T0_dim / m);
+  float alpha = pow(e, 2) / (m * R0_dim * pow(v0_dim, 2));
+  cout << "Constante adimensional, alpha = " << alpha << endl;
+
+  // Radio del círculo y velocidad inicial de la partícula adimensionales
+  float R0 = 1;
+  float v0 = 1;
+
+  cout << "Radio y velocidad iniciales adimensionales: " << R0 << ",\t" << v0 << endl;
   
-  int nBodies = 30000;
-  if (argc > 1) nBodies = atoi(argv[1]);
-  
+
   const float dt = 0.01f; // time step
   const int nIters = 10;  // simulation iterations
 
 
   //Aloco memoria en host
-  int bytes = nBodies*sizeof(Particula);
+  int bytes = N*sizeof(Particula);
 
   float *buf = (float*)malloc(bytes);
   Particula *p = (Particula*)buf;
@@ -63,10 +93,10 @@ int main(const int argc, const char** argv) {
   cudaMalloc(&d_buf_dt, bytes);
   Particula *d_dpdt = (Particula*)d_buf_dt;
 
-  randomizeBodies(buf, 4*nBodies); // Init pos / vel data
+  randomizeBodies(buf, 4*N); // Init pos / vel data
 
 
-  int nBlocks = (nBodies + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  int nBlocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
   double totalTime = 0.0; 
 
   for (int iter = 1; iter <= nIters; iter++) {
@@ -76,18 +106,21 @@ int main(const int argc, const char** argv) {
     cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
     // cudaMemcpy(d_buf_dt, buf_dt, bytes, cudaMemcpyHostToDevice);
 
-    bodyForce<<<nBlocks, BLOCK_SIZE>>>(d_p, d_dpdt, dt, nBodies); // compute interbody forces 
+    bodyForce<<<nBlocks, BLOCK_SIZE>>>(d_p, d_dpdt, dt, N, alpha); // compute interbody forces 
     
   
     cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
     cudaMemcpy(buf_dt, d_buf_dt, bytes, cudaMemcpyDeviceToHost);
 
-
-    for (int i = 0 ; i < nBodies; i++) { // integrate position
+    //Método Leap-Frog
+    for (int i = 0 ; i < N; i++) { // integrate position
       p[i].vx += dt*dpdt[i].vx; p[i].vy += dt*dpdt[i].vy;
       p[i].x += dpdt[i].x*dt;
       p[i].y += dpdt[i].y*dt;
     }
+    //Método de Verlet
+
+
 
     const double tElapsed = GetTimer() / 1000.0;
     if (iter > 1) { // First iter is warm up
@@ -100,11 +133,11 @@ int main(const int argc, const char** argv) {
   double avgTime = totalTime / (double)(nIters-1); 
 
 #ifdef SHMOO
-  printf("%d, %0.3f\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
+  printf("%d, %0.3f\n", N, 1e-9 * N * N / avgTime);
 #else
   //printf("Average rate for iterations 2 through %d: %.3f +- %.3f steps per second.\n",
   //       nIters, rate);
-  printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
+  printf("%d Bodies: average %0.3f Billion Interactions / second\n", N, 1e-9 * N * N / avgTime);
 #endif
   free(buf);
   cudaFree(d_buf);
